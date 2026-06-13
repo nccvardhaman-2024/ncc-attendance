d03af38e562e6e03bdb46901a42346c7d997717e
BOORLA ARJUN <arjunboorla9@gmail.com>
2026-06-14 00:32:51 +0530

    wiring

--- Desktop/ncc_attendance/backend/app.js
+++ Desktop/ncc_attendance/backend/app.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors({ origin: process.env.FRONTEND_URL || 'http://localhost:5173' }));
+app.use(cors({ origin: process.env.FRONTEND_URL}));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
